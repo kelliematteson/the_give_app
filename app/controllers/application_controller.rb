841be7af5a2792c92e708970c3bc6a323cc1c643
Kellie Matteson <kelliematteson@yahoo.com>
2021-03-25 22:37:14 -0700

    trying to debugg the post route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::API
-    #ActionController::Parameters.permit_all_parameters = true
+    # ActionController::Parameters.permit_all_parameters = true
 end
--- app/controllers/gives_controller.rb
+++ app/controllers/gives_controller.rb
@@ -1,40 +1,32 @@
 class GivesController < ApplicationController
     def index
-        gives = Give.all 
-        render json: {status: 200, gives: gives}
+      render(json: { gives: Give.all })
     end
-
+  
     def show
-        give = Give.find(params[:id])
-        render json: {status: 200, give: give}
+      render(json: Give.find(params[:id]))
     end
-    
-    def create 
-        give = Give.new(give_params)
-
-        if give.save
-            render(json: { give: give }, status: 201)
-        else
-            render(json: { give: give }, status: 422)
-        end
+  
+    def create
+        Give.create(params[:give])
+        puts "this is params: #{params[:give]}"
+        
     end
-
+  
     def update
-        give = Give.find(params[:id])
-        give.update(give_params)
-        render(json: { give: give})
+      give = Give.find(params[:id])
+      give.update(give_params)
+      render(json: { give: give })
     end
-
+  
     def destroy
-            give = Give.destroy(params[:id])
-            render(status: 204)
+      give = Give.destroy(params[:id])
+      render(status: 204)
     end
-
-
+  
     private
+  
     def give_params
-        params.required(:give).permit(:give_name, :give_description, :give_image, :giver)
+      params.required(:give).permit(:give_name, :give_description, :give_image, :giver)
     end
-
-
-end
+  end
